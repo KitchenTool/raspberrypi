88a736ce9a40c3507c48c8a11fb480e2d9ceac7a
Jens Egholm <jensegholm@protonmail.com>
2018-06-16 14:05:50 +0000

    Added quotes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+import random
 import time
 from datetime import datetime
 import pytz
@@ -10,6 +11,16 @@ import pytz
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
+
+QUOTES = [
+    "I don't know\n    - Andreas"
+  , "Stupidity is doing the same thing twice,\nand expecting a different result\n    - Kristin"
+  , "Oh, so hot\n    - Andreas"
+  , "DMI is so pessimistic\n    - Andreas"
+  , "I'm SO funny!\n    - Kristin"
+  , "I'm tiiiiiirrreeeeeeeed\n    - Kristin"
+  , "Skildpadde drikker mælk\n    - Kristin"
+  ]
 
 def main():
     # Loop and redraw every 60 seconds
@@ -32,9 +43,17 @@ def main():
 def draw_on_image(timestamp, image):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 185)
-    draw.rectangle((0, 6, 640, 40), fill = 127)
+    #draw.rectangle((0, 6, 640, 40), fill = 127)
+
+    # Clock
     time_text = time_to_text(timestamp)
     draw.text((50, 50), time_text, font = font, fill = 0)
+  
+    # Quotes
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 30)
+    quote = random.choice(QUOTES)
+    w, h = draw.textsize(quote)
+    draw.text(((EPD_WIDTH // 2 - w // 2), 250), quote, font = font, fill = 127)
 
 def time_to_text(timestamp):
    return timestamp.strftime('%H:%M')
