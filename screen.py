b55a3f2e49dc226e6e99d4fd90155950b8a40125
Jens Egholm <jensegholm@protonmail.com>
2019-06-15 21:06:30 +0200

    Added locations

--- screen.py
+++ screen.py
@@ -33,39 +33,50 @@ def get_image():
     timestamp = datetime.now(pytz.timezone('Europe/Copenhagen'))
     weather_json = request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=Copenhagen,DK&appid='+API_KEY).read()
     weather = json.loads(weather_json)
-    condition = weather['weather'][0]['main'] # Weather condition like 'Rain'
-    temperature = weather['main']['temp'] - 273.15 # Temperature converted from fucking Kelvin
+
+    # Prepare location data
+    location_kristin = request.urlopen('https://server.kristinkalt.now.sh/location/Kristin').read().decode()
+    location_jens = request.urlopen('https://server.kristinkalt.now.sh/location/Kristin').read().decode()
 
     # For simplicity, the arguments are explicit numerical coordinates
     image = Image.new('L', (EPD_WIDTH, EPD_HEIGHT), 255) # 255: clear the frame
-    draw_on_image(timestamp, image)
+    draw_on_image(timestamp, weather, location_kristin, location_jens, image)
     return image
 
-def draw_on_image(timestamp, image):
+def draw_on_image(timestamp, weather, location_kristin, location_jens, image):
     draw = ImageDraw.Draw(image)
     #draw.rectangle((0, 6, 640, 40), fill = 127)
 
-    # Date
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 30)
+    # Date and weather
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 25)
     date_text = timestamp.strftime("%A %d/%m/%y")
-    draw.text((200, 10), date_text, font = font, fill = 0)
+    condition = weather['weather'][0]['main'] # Weather condition like 'Rain'
+    temperature = round(weather['main']['temp'] - 273.15, 1) # Temperature converted from fucking Kelvin
+    text = '{} - {}°C {}'.format(date_text, temperature, condition)
+    w, h = draw.textsize(text, font)
+    draw.text(((EPD_WIDTH // 2 - w // 2), 10), text, font = font, fill = 0)
 
     # Time
     font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 185)
     time_text = timestamp.strftime("%H:%M")
     time_text = time_text[:-1] + "?"
-    draw.text((50, 50), time_text, font = font, fill = 0)
+    w, h = draw.textsize(time_text, font)
+    draw.text((EPD_WIDTH // 2 - w //2, 50), time_text, font = font, fill = 0)
+
+    # Location
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 30)
+    kristin_text = "Kristin: {}".format(location_kristin)
+    w, h = draw.textsize(kristin_text, font)
+    draw.text((EPD_WIDTH // 4 - w // 2, 260), kristin_text, font = font, fill = 0)
+    jens_text = "Jens: {}".format(location_jens)
+    w, h = draw.textsize(jens_text, font)
+    draw.text((EPD_WIDTH - (EPD_WIDTH // 4) - w // 2, 260), jens_text, font = font, fill = 0)
 
     # Quotes
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 30)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 20)
     quote = random.choice(QUOTES)
-    w, h = draw.textsize(quote)
-    draw.text(((EPD_WIDTH // 2 - w // 2), 250), quote, font = font, fill = 127)
-
-    # Weather
-    #weather_image = Image.open().convert('L')
-    #inverted_weather = ImageOps.invert(weather_image)
-    #image.paste(inverted_weather, (20, 230))
+    w, h = draw.textsize(quote, font)
+    draw.text(((EPD_WIDTH // 2 - w // 2), 320), quote, font = font, fill = 127)
 
 if __name__ == "__main__":
     get_image().show()
