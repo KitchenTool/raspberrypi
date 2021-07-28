c39921f91aaaa9ea70ed741541544ac54d96e08d
Jens Egholm <jensegholm@protonmail.com>
2021-07-28 21:22:05 +0200

    Cached entry of the day

--- wikipedia.py
+++ wikipedia.py
@@ -1,12 +1,22 @@
 import bs4
 import re
-import urllib
+import urllib, urllib.parse, urllib.request
 import datetime
 import random
 
 REF_REGEX = re.compile(r"\[\d+\]")
+DAY_FILE = "today.txt"
 
 def random_event():
+    try:
+        with open(DAY_FILE, 'r') as fp:
+            items = fp.readlines()
+        text = random.choice(items).strip()
+        return re.sub(REF_REGEX, "", text)
+    except:
+        return "Today is a great day!"
+
+def cache_day():
     date = datetime.date.today().strftime("%B %d")
     date = urllib.parse.quote(date)
     url = "https://en.wikipedia.org/wiki/" + date
@@ -22,6 +32,9 @@ def random_event():
         if sibling.name == "ul":
             items += sibling.find_all("li")
             
-    event = random.choice(items)
-    text = event.text
-    return re.sub(REF_REGEX, "", text)
+    lines = '\n'.join([x.text for x in items])
+    with open(DAY_FILE, 'w') as fp:
+        fp.write(lines)
+
+if __name__ == "__main__":
+    cache_day()
